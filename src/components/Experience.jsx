a301c2303635c3493201097b9f7e290692b0f73b
Barel Hajbi <72247396+barel26497@users.noreply.github.com>
2026-08-05 19:01:54 +0300

    Update thumbnail paths in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,14 +7,14 @@ const experiences = [
     company: "Youleap",
     location: "Kadima Zoran, Israel",
     period: "July 2026 - Current",
-    thumbnail: "/experience/youleap-logo.png",
+    thumbnail: "/Images/youleap-logo.png",
   },
   {
     role: "RF Engineer",
     company: "Beeper",
     location: "Ramat Gan, Israel",
     period: "May 2019 - December 2022",
-    thumbnail: "/experience/beeper-logo.png",
+    thumbnail: "/Images/beeper-logo.jpeg",
     points: [
       "Troubleshot and maintained complex technical systems in the field. Worked cross-functionally to resolve issues and support system reliability.",
     ],
@@ -24,7 +24,7 @@ const experiences = [
     company: "IDF",
     location: "Jerusalem, Israel",
     period: "February 2016 - May 2019",
-    thumbnail: "/experience/idf-logo.png",
+    thumbnail: "/Images/idf-logo.png",
     points: [
       "Led C4I operations and managed critical military communication systems. Oversaw advanced tech platforms including Elbit DAP, Motorola, and WiMAX, supporting field missions.",
     ],
@@ -34,7 +34,7 @@ const experiences = [
     company: "Optinize",
     location: "Tel Aviv, Israel",
     period: "January 2015 - February 2016",
-    thumbnail: "/experience/optinize-logo.png",
+    thumbnail: "/Images/optinize-logo.png",
     points: [
       "Built and customized marketing content using HTML and CSS. Collaborated with international clients to support tech-driven campaigns.",
     ],
